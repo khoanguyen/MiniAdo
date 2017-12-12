c58cdd2969e593e8b3463d0568ec39fe66a25946
knguyen <minhkhoa4783@gmail.com>
2017-12-11 17:27:26 -0800

    Start Mapping implementation

---
+++ C3R.MiniAdo/Mapping/IMappingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C3R.MiniAdo.Mapping
+{
+    public interface IMappingService
+    {
+        object Map(object source, Type targetType);
+        void Populate(object source, object target);
+    }
+}
